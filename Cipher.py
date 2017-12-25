dba82b1fa3fca2ecae58fcda22dc04353d510b3a
Frestho <34761944+Frestho@users.noreply.github.com>
2017-12-24 21:35:44 -0800

    Caps handling for encode program
    Added code for handling capital letters, but doesn't have the comments from the master branch yet.

--- Cipher.py
+++ Cipher.py
@@ -1,23 +1,24 @@
 import random
 #sets the caesar cipher key
 key = random.randint (1, 25)
-#gives you the key, remove this for it to be secret.
 print (key)
 letters = 'abcdefghijklmnopqrstuvwxyz'
+caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 phrase = input('Type a phrase to be encoded: ')
-#defines the variable that will store the encoded phrase
 encoded = ''
-#main function that encrypts your phrase under the key. only letters are affected
-def encode():
-    global encoded
-    for place in range(0, len(phrase)):
-        #finds the place that the letter is on in the letters variable
-        placeInLetters = letters.find(phrase[place])
-        #if not a letter, don't do anything
-        if not(placeInLetters < 25 and placeInLetters > -1):
-            encoded = encoded + phrase[place]
-        #and if it's a letter, encode it.
-        else:
-            encoded = encoded + letters[(placeInLetters+key) % 26]
-encode()
+for place in range(0, len(phrase)):
+    inCaps = caps.find(phrase[place])
+    if inCaps < 26 and inCaps > -1: #should only be true if there was a capital letter
+            phrase2 = ''#this variable handles some temporary stuff
+            for i in range (0, len(phrase)): #goes through the user input, ready to replace the capital letter with a lowercase
+                if i == place: #am I at the capital letter? if so add it to code 2 from the lowercase letter list
+                    phrase2 += letters[inCaps]
+                else: #otherwise, do nothing different
+                    phrase2 += phrase[i]
+            phrase = phrase2 
+    placeInLetters = letters.find(phrase[place])
+    if not(placeInLetters < 25 and placeInLetters > -1):
+        encoded = encoded + phrase[place]
+    else:
+        encoded = encoded + letters[(placeInLetters+key) % 26]
 print (encoded)    
